408c13b2a3d43e1c0ceecf2688704bfb97f0e38e
Rajankumar001 <chaudharyrajan387@gmail.com>
2025-02-28 15:26:52 +0530

    changes tracked

--- src/components/Screens/DashboardScreen.js
+++ src/components/Screens/DashboardScreen.js
@@ -39,6 +39,36 @@ const DashboardScreen = () => {
             <div className="grid-item">Item 6</div>
           </div>
        </div>
+       <div className='Dashboard-Screen-four'>
+       <div className="grid-container">
+            <div className="grid-item">Item 1</div>
+            <div className="grid-item">Item 2</div>
+            <div className="grid-item">Item 3</div>
+            <div className="grid-item">Item 4</div>
+            <div className="grid-item">Item 5</div>
+            <div className="grid-item">Item 6</div>
+          </div>
+       </div>
+       <div className='Dashboard-Screen-four'>
+       <div className="grid-container">
+            <div className="grid-item">Item 1</div>
+            <div className="grid-item">Item 2</div>
+            <div className="grid-item">Item 3</div>
+            <div className="grid-item">Item 4</div>
+            <div className="grid-item">Item 5</div>
+            <div className="grid-item">Item 6</div>
+          </div>
+       </div>
+       <div className='Dashboard-Screen-four'>
+       <div className="grid-container">
+            <div className="grid-item">Item 1</div>
+            <div className="grid-item">Item 2</div>
+            <div className="grid-item">Item 3</div>
+            <div className="grid-item">Item 4</div>
+            <div className="grid-item">Item 5</div>
+            <div className="grid-item">Item 6</div>
+          </div>
+       </div>
       </div>
     </>
   )
